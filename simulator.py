a6fc43f548b57914d257eb5e40a04afece1c2cad
CuriousPelican <77733637+CuriousPelican@users.noreply.github.com>
2024-05-11 22:16:31 +0200

    Bottle intialization & code functionalization

--- simulator.py
+++ simulator.py
@@ -38,7 +38,7 @@ Rt /= 1000
 z_ret /= 1000
 mb /= 1000
 P *= 100000
-Vl /= 1000
+Vl0 /= 1000
 T += 273.15
 P_atm *= 100000
 
@@ -50,10 +50,8 @@ rho_air = 1.292*273.15/T # 1.292 kg/m^3 pour T=0°C
 
 
 
-############################## Bouteille ##############################
+############################## Géométrie bouteille & Init ##############################
 # Referentiel Bouteille - z est la distance axiale par rapport à la sotrie de la tuyère (flux supposé unidirectionnel)
-
-H = 0 # H(t) hauteur interface air-liquide
 
 # Renvoie le rayon a z donné - basé sur puis arctan entre -param_tan et param_tan et 
 def R(z):
@@ -73,7 +71,6 @@ def A(z):
 
 V_ret = integrate.quad(A, 0, z_ret)[0] # volume (en L) de z=0 a z=z_ret
 print(f"V_ret = {round(V_ret*1000, 3)} L (volume de z=0 à z=z_ret={z_ret*1000} mm)")
-
 z_max = (Vb-V_ret)/(np.pi*Rb**2) + z_ret # hauteur (en m) de la bouteille
 print(f"Donc z_max={round(z_max*100, 1)}cm (hauteur totale bouteille)")
 
@@ -81,33 +78,51 @@ print(f"Donc z_max={round(z_max*100, 1)}cm (hauteur totale bouteille)")
 # plt.plot([R(z) for z in np.linspace(0,z_ret,100)])
 # plt.show()
 
-# Renvoie rho a z donné & H fixé
-def rho(z):
-  if z>H:
+# Renvoie H, hauteur de liquide avec une précision e
+# pour une volume de liquide V donné par recherche par dichotomie sur V
+def H(V,e):
+  def f(z):
+    return integrate.quad(A, 0, z)[0]-V
+  a,b = 0,z_max
+  while (f(b)-f(a))>e:
+    m = (a+b)/2
+    if f(m)*f(b)>0:
+      b = m
+    else:
+      a = m
+  return m,f(m)
+
+H0 = H(Vl0,10e-4)[0] # H(t) hauteur interface air-liquide
+
+# Renvoie rho à z & H donnés
+def rho(z,H):
+  if z>=H:
     return rho_air
   else:
     return rho_l
 
-# Masse linéique axiale rho(z)*A(z) à H fixé
-def dm(z):
-  return rho(z)*A(z)
+# Masse linéique axiale rho(z,H)*A(z)
+def dm(z,H):
+  return rho(z,H)*A(z)
 
-# Renvoie la masse totale de la fusée à H fixé
-def m_tot():
-  return mb + integrate.quad(dm, 0, z_max)[0]
+# Renvoie la masse totale de la fusée à H donné
+def m_tot(H):
+  return mb + integrate.quad(dm, 0, z_max, args=(H))[0]
 
-# Renvoie le volume de liquide à H fixe
-def Vl():
-  return integrate.quad(dm, 0, H)[0]
+# Renvoie le volume de liquide dans la fusée à H donné
+def Vl(H):
+  return integrate.quad(dm, 0, H, args=(H))[0]
+
+# Referentiel Terrestre - y est la distance verticale entre le sol et la fusée (vol supposé purement vertical)
+y = 0 # y(t) altitude fusée
+v = 0 # v(t)
+a = 0 # a(t)
+
+
 
 ############################## 4 Later ##############################
 
 u = "hum" # u(z,t) vitesse axiale fluide/bouteille
-
-# Referentiel Terrestre - y est la distance verticale entre le sol et la fusée (vol supposé purement vertical)
-y = [] # y(t) altitude fusée
-v = [] # v(t)
-a = [] # a(t)
 
 # Renvoie la norme de la force de trainée
 def F_drag(Cd,rho,A,v):
