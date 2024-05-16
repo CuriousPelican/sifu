564079488c28ada7a492d64f167e7eb39d0cd9ea
CuriousPelican <77733637+CuriousPelican@users.noreply.github.com>
2024-05-17 00:59:36 +0200

    Change in bottle characteristics determination method: introduced discretisation

--- simulator.py
+++ simulator.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
+import time
 
 ############################## Paramètres ##############################
 
@@ -10,6 +11,7 @@ import scipy.integrate as integrate
 # Les entrées sont ensuites converties en unités SI et toutes les variables et le programme fonctionnent avec des unités SI.
 
 # Simulation
+dz = 0.01 # pas (en mm) utilisé pour l'integration du volume de la bouteille, defaut 10µm
 
 # Bouteille (lorsque l'on parle de la la bouteille, est a comprendre la chambre de pression & la tuyère)
 Vb = 1 # volume (en L) de la bouteille
@@ -38,6 +40,7 @@ P_atm = 1.01325 # pression atmosphérique (en bar)
 ############################## Conversion SI & Constantes ##############################
 
 # Conversions SI
+dz /= 1000
 Vb /= 1000
 Rb /= 1000
 Rt /= 1000
@@ -60,7 +63,7 @@ rho_air = 1.292*273.15/T # 1.292 kg/m^3 pour T=0°C
 
 
 
-############################## R(z), A(z), z_max ##############################
+############################## R(z), A(z) ##############################
 
 # Referentiel Bouteille - z est la distance axiale par rapport à la sotrie de la tuyère (flux supposé unidirectionnel)
 
@@ -80,20 +83,79 @@ def R(z):
 def A(z):
   return np.pi*R(z)**2
 
-V_ret = integrate.quad(A, 0, z_ret)[0] # volume (en L) de z=0 a z=z_ret
-print(f"V_ret = {round(V_ret*1000, 3)} L (volume de z=0 à z=z_ret={z_ret*1000} mm)")
-z_max = (Vb-V_ret)/(np.pi*Rb**2) + z_ret # hauteur (en m) de la bouteille
-print(f"Donc z_max={round(z_max*100, 1)}cm (hauteur totale bouteille)")
 # Test courbe rétrécissement
 # plt.plot([R(z) for z in np.linspace(0,z_ret,100)])
 # plt.show()
 
+############################## DISCRETISATION H(Vw,e), rho(z), m_tot(H), Vw(H) ##############################
+
+# Methode simpson & discretisation z
+# Renvoie la liste de z, A(z) & V(z) en partant de 0 discretisés, ainsi que V_ret & z_max
+def init_bouteille():
+  # Calculs avant rétrécissement
+  z_liste1 = np.arange(0,z_ret+dz,dz)
+  A_liste1 = np.array([A(z) for z in z_liste1])
+  V_liste1 = integrate.cumulative_simpson(A_liste1,dx=dz) # liste des volumes, V[i] = volume entre 0 et z_liste1[i]
+
+  # V_ret & z_max
+  V_ret = V_liste1[-1] # volume (en m^3) de z=0 a z=z_ret
+  print(f"V_ret = {round(V_ret*1000, 3)} L (volume de z=0 à z=z_ret={z_ret*1000} mm)")
+  z_max = (Vb-V_ret)/(np.pi*Rb**2) + z_ret # hauteur (en m) de la bouteille
+  print(f"Donc z_max={round(z_max*100, 1)}cm (hauteur totale bouteille)")
+
+  # Calculs après rétrécissement
+  z_liste2 = np.arange(z_ret+dz,z_max+dz,dz)
+  A_liste2 = np.array([A(z) for z in z_liste2])
+  V_liste2 = [np.pi*Rb**2*(z-z_ret) for z in z_liste2]
+
+  z_liste = np.concatenate((z_liste1,z_liste2))
+  A_liste = np.concatenate((A_liste1,A_liste2))
+  V_liste = np.concatenate((V_liste1,V_liste2))
+
+  return (z_liste,A_liste,V_liste,V_ret,z_max)
+
+(z_liste,A_liste,V_liste,V_ret,z_max) = init_bouteille()
+
+############################## Lanceur & (y,v,a) ##############################
+
+A_TO = np.pi*R_TO**2 # section ext tube lancement
+A_TI= np.pi*R_TI**2 # section int tube lancement
+
+# Referentiel Terrestre - y est la distance verticale entre le sol et la fusée (vol supposé purement vertical)
+y = 0 # y(t) altitude fusée
+v = 0 # v(t)
+a = 0 # a(t)
 
 
-############################## H(Vw,e), rho(z), m_tot(H), Vw(H) ##############################
+
+############################## 4 Later ##############################
+
+u = "hum" # u(z,t) vitesse axiale fluide/bouteille
+
+# Renvoie la norme de la force de trainée
+def F_drag(Cd,rho,A,v):
+  return 1/2*Cd*rho*A*v**2
+
+# Renvoie la norme de la force de poussée
+def F_thrust():
+  return "hum"
+
+
+
+############################## UNUSED ##############################
+
+# UNUSED - FUNCTION OBJECT H(Vw,e), rho(z), m_tot(H), Vw(H)
+"""
+# Methode fonctionelle avec scipy
+
+V_ret = integrate.quad(A, 0, z_ret)[0] # volume (en m^3) de z=0 a z=z_ret
+print(f"V_ret = {round(V_ret*1000, 3)} L (volume de z=0 à z=z_ret={z_ret*1000} mm)")
+z_max = (Vb-V_ret)/(np.pi*Rb**2) + z_ret # hauteur (en m) de la bouteille
+print(f"Donc z_max={round(z_max*100, 1)}cm (hauteur totale bouteille)")
 
 # Renvoie H, hauteur de liquide avec une précision e
 # pour une volume de liquide V donné par recherche par dichotomie sur V
+
 def H(Vw,e):
   def f(z):
     return integrate.quad(A, 0, z)[0]-Vw
@@ -125,29 +187,16 @@ def m_tot(H):
 # Renvoie le volume de liquide dans la fusée à H donné
 def Vw(H):
   return integrate.quad(dm, 0, H, args=(H))[0]
+"""
 
-
-
-############################## Lanceur & (y,v,a) ##############################
-
-A_TO = np.pi*R_TO**2 # section ext tube lancement
-A_TI= np.pi*R_TI**2 # section int tube lancement
-
-# Referentiel Terrestre - y est la distance verticale entre le sol et la fusée (vol supposé purement vertical)
-y = 0 # y(t) altitude fusée
-v = 0 # v(t)
-a = 0 # a(t)
-
-
-
-############################## 4 Later ##############################
-
-u = "hum" # u(z,t) vitesse axiale fluide/bouteille
-
-# Renvoie la norme de la force de trainée
-def F_drag(Cd,rho,A,v):
-  return 1/2*Cd*rho*A*v**2
-
-# Renvoie la norme de la force de poussée
-def F_thrust():
-  return "hum"
+# UNUSED - Intégrale du rayon entre a et b (objectif était le volume mais expression analytique trop complexe)
+def R_int(a,b):
+  param_tan = 1.8 # meilleurs parametres trouvés pour correspondre a mes bouteilles
+  z_goulot = 0.26*z_ret
+  # Primitive de R
+  def G(z):
+    a = -(z_ret-z_goulot)*np.log((param_tan**2+1)*(z_ret**2+z_goulot**2)+2*z_ret*z_goulot*(param_tan**2-1)+4*param_tan**2*z*(z-z_goulot-z_ret))
+    b = 2*param_tan*(z_ret+z_goulot-2*z)*np.arctan(param_tan*(z_ret+z_goulot-2*z)/(z_ret-z_goulot))
+    c = (Rb+Rt)*z/2
+    return (Rb-Rt)*(a+b) + c
+  return(G(b)-G(a))
